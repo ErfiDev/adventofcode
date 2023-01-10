35d6745d4ae98f80e3f123cdc73ec66be2e1c1a0
erfanhanifezade <erfanhanifezade@gmail.com>
2023-01-10 19:43:03 +0330

    working day 5

--- 2022/5_supply_stacks/app.go
+++ 2022/5_supply_stacks/app.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
-	"os"
+	"strings"
 )
 
 type Stack struct {
+	name   string
 	crates []Crate
 }
 
@@ -24,25 +26,68 @@ type Command struct {
 	to    int
 }
 
-func main() {
-	readFile("./puzzle")
+//go:embed puzzle
+var puzzle string
+
+func init() {
+	puzzle = strings.TrimRight(puzzle, "\n")
+	if len(puzzle) == 0 {
+		panic("puzzle file are empty!")
+	}
 }
 
-func readFile(path string) []Stack {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
+func main() {
+	stacks, commands := readAndSplit(puzzle)
+
+	for _, c := range commands {
+		
+	}
+}
+
+func readAndSplit(pzl string) ([]Stack, []Command) {
+	splitedPzl := strings.Split(pzl, "\n\n")
+
+	firstPart := splitedPzl[0]
+	var data [][]string
+
+	for _, p := range strings.Split(firstPart, "\n") {
+		data = append(data, strings.Split(p, ""))
 	}
 
-	defer file.Close()
+	col, row := len(data[0]), len(data)
 
-	var data []byte
-	_, err = file.Read(data)
-	if err != nil {
-		log.Fatal(err)
+	var stacks []Stack
+
+	// extracting stacks
+	for i := 0; i <= col-1; i++ {
+		if data[row-1][i] != " " {
+			stack := Stack{}
+			stack.name = data[row-1][i]
+			for j := row - 2; j >= 0; j-- {
+				name := data[j][i]
+				stack.crates = append(stack.crates, Crate{name})
+			}
+
+			stacks = append(stacks, stack)
+		}
 	}
 
-	fmt.Println(data)
+	var commands []Command
 
-	return nil
+	secondPart := splitedPzl[1]
+	byLine := strings.Split(secondPart, "\n")
+
+	// extracting commands
+	for _, l := range byLine {
+		cmd := Command{}
+
+		_, err := fmt.Sscanf(l, "move %d from %d to %d", &cmd.index, &cmd.from, &cmd.to)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		commands = append(commands, cmd)
+	}
+
+	return stacks, commands
 }
